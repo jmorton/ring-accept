0b2c737e882533decd058077ddb183d08b36e4b3
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-07-06 09:13:24 -0500

    WIP: Explore defaccept idea.
    This is a first step into providing users with a means of mapping content-types to rendering functions.
    
    Matching a media-range with wildcards (e.g. text/*) is simple in principle, but nuanced in practice. First, I haven't been able to find something suitable for handling mime-type matching that is comprehensive; specifically, handling mime-types with vendor trees (e.g. application/vnd.acme.v1.0+json) against media-ranges like application/json.

---
+++ src/ring/util/accept.clj
@@ -0,0 +1,24 @@
+(ns ring.util.accept)
+
+(defn best-match
+  "Choose best renders given accept header of request"
+  ;; TODO: Support wild card matching; hopefully this doesn't
+  ;; need to be implemented from scratch.
+  [request renders]
+  (let [accept (request :accept)
+        accept-media (into (sorted-set) (map :media-type accept))
+        render-media (into (sorted-set) (keys renders))]
+    (first (clojure.set/intersection accept-media render-media))))
+
+(defn fallback
+  "Default response when no acceptable content can be produced"
+  [response]
+  (merge response {:status 406}))
+
+(defmacro defaccept [name & renders]
+  `(defn ~name [request# response#]
+     (let [renders# (hash-map ~@renders)
+           best-media# (best-match request# renders#)
+           best-render-fn# (renders# best-media# ~fallback)]
+       (->> (assoc response# :content-type best-media#)
+            (best-render-fn#)))))
