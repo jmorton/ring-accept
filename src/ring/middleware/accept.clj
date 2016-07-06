d527bc6ced67188c6cdf50fbce161b49032c45bd
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-07-06 00:30:31 -0500

    Build map from parsed accept header.
    Produce a list of maps containing parts of the accept header in order to support more nuanced matching. This includes the type, subtype, quality, and extensions, as well as the implicity specificity of a media-range. The list of media-range maps is sorted using both quality and specificity.
    
    This changes the original data structure of the value this middleware associates with the request, but given the current state of the middleware, it seems reasonable to do at this point...

--- src/ring/middleware/accept.clj
+++ src/ring/middleware/accept.clj
@@ -1,44 +1,116 @@
 (ns ring.middleware.accept
+  "Functions for parsing Accept header media-ranges."
   (:require [clojure.string :as str]
             [ring.util.parsing :as p]))
 
 (def ^:private re-wildcard #"\*")
 
+;; Parses quality portion of a media range, e.g. ;q=0.9
 (def ^:private re-qvalue #"0(?:\.\d{0,3})?|1(?:\.0{0,3})?")
-
-(def ^:private re-parameter
-  (re-pattern (str "\\s*;\\s*" p/re-token "=" p/re-value)))
 
 (def ^:private re-accept-params
   (re-pattern (str "\\s*;\\s*" "q=(" re-qvalue ")")))
 
+;; Parses the extension portion of a media range, e.g. ;foo=1;bar=2
+(def ^:private re-parameter
+  (re-pattern (str "\\s*;\\s*" p/re-token "=" p/re-value)))
+
+;; Splits each extension into match/key/value.
+(def ^:private re-extensions
+  (re-pattern "(?<=;)([^=]+)=([^;]+)"))
+
+;; Technically, this pattern is incorrect because it matches
+;; an erroneous media-range such as "*/plain", but a more
+;; complicated pattern introduces complexities when parsing
+;; ranges and subranges.
 (def ^:private re-media-range
-  (re-pattern (str "(" re-wildcard "/" re-wildcard
-                   "|" p/re-token  "/" re-wildcard
-                   "|" p/re-token  "/" p/re-token ")"
+  (re-pattern (str "((" re-wildcard "|" p/re-token ")"
+                   "/(" re-wildcard "|" p/re-token "))"
                    "((?:" re-parameter ")*?)")))
 
+;; Parses one entire media-range.
 (def ^:private re-accept-value
-  (re-pattern (str "(" re-media-range ")"
+  (re-pattern (str "(?:" re-media-range ")"
                    "(?:(" re-accept-params ")"
                    "(" re-parameter ")*)?")))
 
+;; Parses the entire accept header, splitting multiple media-ranges.
 (def ^:private re-accept-header
   (re-pattern (str "(?<=^|,)\\s*(" re-media-range ")\\s*(?:,|$)")))
 
-(defn- parse-accept [header-value]
-  (let [[_ type _ _ _ _ q] (re-matches re-accept-value header-value)]
-    {:media-type type
+(defn- parse-accept
+  ""
+  [header-value]
+  (let [[_ media-type type subtype parameters _ _ q] (re-matches re-accept-value header-value)]
+    {:media-type media-type
+     :type type
+     :subtype subtype
+     :parameters parameters
+     :specificity 0
      :quality (Double/parseDouble (or q "1"))}))
 
-(defn- parse-header [header]
+(defn- parse-header
+  ""
+  [header]
   (map second (re-seq re-accept-header header)))
 
-(defn accept-request [request]
+;; According to RFC7231, values are case insensitive.
+;; In order to support consistent matching, strings
+;; are converted to lower case.
+;; https://tools.ietf.org/html/rfc7231#section-3.1.1.1
+(defn normalize
+  "Make media-range consistently lower-case."
+  [media-range]
+  (clojure.string/lower-case media-range))
+
+(defn +specificity
+  "Used when sorting indidual accept values."
+  [accept]
+  (assoc accept :specificity
+         (cond->> 0
+           (not-empty (accept :extensions)) (+ 4)
+           (not= (accept :subtype) "*") (+ 2)
+           (not= (accept :type) "*") (+ 1))))
+
+(defn +extensions
+  "Convert a single extensions string into a hash-map of strings."
+  [accept]
+  (if-let [params (:parameters accept)]
+    (->> params
+         (re-seq re-extensions)
+         (map rest)
+         (map vec)
+         (reduce conj {})
+         (assoc accept :extensions))
+    (assoc accept :extensions {})))
+
+(def ^:private re-extensions
+  (re-pattern "(?<=;)([^=]+)=([^;]+)"))
+
+(->> (re-seq re-extensions ";foo=1;bar=2")
+     (map rest)
+     (map vec)
+     (reduce conj {}))
+
+(defn prioritize
+  "Sort media-types by quality and specificity, most to least."
+  [accepts]
+  (sort-by (juxt :quality :specificity) #(compare %2 %1) accepts))
+
+(def accept-xf (comp (map normalize)
+                     (map parse-accept)
+                     (map +extensions)
+                     (map +specificity)))
+
+(defn accept-request
+  ""
+  [request]
   (if-let [header (get-in request [:headers "accept"])]
     (let [values (parse-header header)
-          accept (into [] (map parse-accept values))]
+          accept (prioritize (sequence accept-xf values))]
       (assoc request :accept accept))))
 
-(defn wrap-accept [handler]
+(defn wrap-accept
+  ""
+  [handler]
   (comp handler accept-request))
--- test/ring/middleware/accept_test.clj
+++ test/ring/middleware/accept_test.clj
@@ -1,7 +1,49 @@
 (ns ring.middleware.accept-test
-  (:use clojure.test
-        ring.middleware.accept))
+  (:require [clojure.test :refer :all]
+            [ring.middleware.accept :as accept]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-accept-test
+  (testing "single media-range without quality"
+    (let [media-range "text/html"
+          request {:headers {"accept" media-range}}
+          results (-> request accept/accept-request :accept first)]
+      (is (= 1.0 (results :quality)))
+      (is (= "text" (results :type)))
+      (is (= "html" (results :subtype)))
+      (is (= "text/html" (results :media-type)))))
+  (testing "single media-range with quality"
+    (let [media-range "text/html;q=0.5"
+          request {:headers {"accept" media-range}}
+          results (-> request accept/accept-request :accept first)]
+      (is (= 0.5 (results :quality)))
+      (is (= "text" (results :type)))
+      (is (= "html" (results :subtype)))
+      (is (= "text/html" (results :media-type)))))
+  (testing "media-range quality sorting"
+    (let [media-range "text/baz;q=0.5, text/foo, text/bar;q=0.9"
+          request {:headers {"accept" media-range}}
+          results (:accept (accept/accept-request request))]
+      (is (= "text/foo" (:media-type (nth results 0))))
+      (is (= "text/bar" (:media-type (nth results 1))))
+      (is (= "text/baz" (:media-type (nth results 2))))))
+  (testing "media-range specificity sorting"
+    (let [media-range "text/*, text/html, text/html;level=1, */*"
+          request {:headers {"accept" media-range}}
+          results (:accept (accept/accept-request request))]
+      (is (= {"level" "1"} (:extensions (nth results 0))))
+      (is (= "text/html" (:media-type (nth results 1))))
+      (is (= "text/*" (:media-type (nth results 2))))
+      (is (= "*/*" (:media-type (nth results 3))))))
+  (testing "media-range quality and specificity sorting"
+    (let [media-range "text/*;q=0.9, text/foo;q=0.9, text/bar"
+          request {:headers {"accept" media-range}}
+          results (:accept (accept/accept-request request))]
+      (is (= "text/bar" (:media-type (nth results 0))))
+      (is (= "text/foo" (:media-type (nth results 1))))
+      (is (= "text/*" (:media-type (nth results 2))))))
+  (testing "equivalance irrespective of case"
+    (let [media-range "Text/HTML;Charset=UTF-8"
+          request {:headers {"accept" media-range}}
+          results (first (:accept (accept/accept-request request)))]
+      (is (= "text/html" (:media-type results)))
+      (is (= {"charset" "utf-8"} (:extensions results))))))
