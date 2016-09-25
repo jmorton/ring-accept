4461b679f9029d7d4b7f1826feaeb538a56c5adc
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-09-25 16:32:44 -0500

    Bump to version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ring/ring-accept "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject jmorton/ring-accept "0.2.0-SNAPSHOT"
+  :description "Ring middleware and utils for using accept headers"
+  :url "http://gihub.com/jmorton/ring-accept"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
