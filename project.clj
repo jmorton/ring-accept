ce9853a658cd65eaec9b9c737c5b5be6211ab57e
Jonathan Morton <jon.morton@gmail.com>
2016-11-10 09:31:34 -0600

    Update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jmorton/ring-accept "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Accept header handling and helpers"
+  :url "http://github.com/jmorton/ring-accept"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
